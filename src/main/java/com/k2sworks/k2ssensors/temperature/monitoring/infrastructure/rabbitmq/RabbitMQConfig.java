1e9e450a22696496fdfd812d64005b17ec29c8c5
Kleberson Santos <kleberson2santos@gmail.com>
2025-05-15 23:56:21 -0400

    Add Communication with messaging and RabbitMQ

---
+++ src/main/java/com/k2sworks/k2ssensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,52 @@
+package com.k2sworks.k2ssensors.temperature.monitoring.infrastructure.rabbitmq;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_PROCESS_TEMPERATURE = "temperature-monitoring.process-temperature.v1.q";
+    public static final String QUEUE_ALERTING = "temperature-monitoring.process-alerting.v1.q";
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public Queue queueProcessTemperature() {
+        return QueueBuilder.durable(QUEUE_PROCESS_TEMPERATURE).build();
+    }
+
+    @Bean
+    public Queue queueAlerting() {
+        return QueueBuilder.durable(QUEUE_ALERTING).build();
+    }
+
+
+    public FanoutExchange exchange() {
+        return ExchangeBuilder.fanoutExchange("temperature-processing.temperature-received.v1.e").build();
+    }
+
+    @Bean
+    public Binding bindingProcessTemperature() {
+        return BindingBuilder.bind(queueProcessTemperature()).to(exchange());
+    }
+
+    @Bean
+    public Binding bindingAlerting() {
+        return BindingBuilder.bind(queueAlerting()).to(exchange());
+    }
+
+}
